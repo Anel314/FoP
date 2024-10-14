bc9744062a4ba2e6e10d60a1074878e419861edd
Anel Brcaninovic <116464323+AnelBrcaninovic@users.noreply.github.com>
2024-10-14 13:29:38 +0200

    First 2 tasks

---
+++ Week 3/main.py
@@ -0,0 +1,12 @@
+print("------------------TASK 1-------------------------")
+
+a,b = int(input("A:")),int(input("B:"))
+print(a+b) if a>b else print(a*b)
+
+
+
+
+print("------------------TASK 2-------------------------")
+
+for i in range(100,400+1,2):
+    print(i, end=" ")
