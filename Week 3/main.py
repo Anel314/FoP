0b599a5e608af1a01dcaff7c4ebe234774ef0b40
Anel Brcaninovic <116464323+AnelBrcaninovic@users.noreply.github.com>
2024-10-14 13:54:01 +0200

    Dunno

--- Week 3/main.py
+++ Week 3/main.py
@@ -1,12 +1,21 @@
-print("------------------TASK 1-------------------------")
+# print("------------------TASK 1-------------------------")
 
-a,b = int(input("A:")),int(input("B:"))
-print(a+b) if a>b else print(a*b)
+# a,b = int(input("A:")),int(input("B:"))
+# print(a+b) if a>b else print(a*b)
 
+# print("------------------TASK 2-------------------------")
 
+# for i in range(100,400+1,2):
+#     print(i, end=" ")
 
+# print("------------------IDK-------------------------")
+# n=12
+# for i in range(n):
+#     print("* "*i)
+# for i in range(n):
+#     print("* " * (n-i))
 
-print("------------------TASK 2-------------------------")
+# a,b = int(input("A:")),int(input("B:"))
 
-for i in range(100,400+1,2):
-    print(i, end=" ")
+# for i in range(1,b+1):
+#     print(f"{i}*{a}={i*a}")
